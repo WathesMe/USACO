b3baa74049d2080ce5a30bd5aa3ddeb0309c0e3e
Wathes <wlu6747@gmail.com>
2025-12-27 16:20:04 -0800

    Fixed The Cow Signal Problem. Any value of K now works

--- Bronze/The_Cow_Signal.py
+++ Bronze/The_Cow_Signal.py
@@ -1,3 +1,6 @@
+#Question Link: https://usaco.org/index.php?page=viewproblem2&cpid=665
+#Solution Link: https://usaco.guide/problems/usaco-665-the-cow-signal/solution
+
 list1 = list(map(int, input().split()))
 M = list1[0] #Vertical Size
 N = list1[1] #Horizontal Size
@@ -6,15 +9,16 @@ signal = list() #Signal Input
 grid = list()
 grid2 = list()
 
-#arr = [[0]*N for _ in range(M)] #2D Array
-
 for j in range(M):
     grid.append(list(input().strip()))
-    print(grid)
-
-print(grid)
 
 arr2 = [[0]*(N*K) for _ in range(M*K)] #Scaled 2D Array
 
-for k in range(M):
-    grid2.copy(arr2)
+for i in range(M):
+    for j in range(N):
+        for k in range(K):
+            for l in range(K):
+                arr2[K*i + k][K*j + l] = grid[i][j]
+
+for i in range(M*K):
+    print(*arr2[i])
